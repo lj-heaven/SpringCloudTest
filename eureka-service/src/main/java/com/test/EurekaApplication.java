3aa4cfa9e6c19c8c2373b7065a0a58797c984f77
heaven <953361478@qq.com>
2023-03-24 00:37:43 +0800

    测试学习

--- eureka-service/src/main/java/com/test/EurekaApplication.java
+++ eureka-service/src/main/java/com/test/EurekaApplication.java
@@ -2,7 +2,13 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 将项目作为SpringCloud中的注册中心
+ * @author Liao
+ */
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
     public static void main(String[] args) {
