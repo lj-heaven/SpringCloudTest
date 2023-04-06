7e15eb6a15a4c3ff761a7e8207d2084a2626f0d9
heaven <953361478@qq.com>
2023-04-07 01:19:17 +0800

    测试学习

--- borrow-service/src/main/java/com/test/controller/BorrowController.java
+++ borrow-service/src/main/java/com/test/controller/BorrowController.java
@@ -1,5 +1,6 @@
 package com.test.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.test.entity.UserBorrowDetail;
 import com.test.service.BorrowService;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 
 /**
  * 借阅信息服务
@@ -18,8 +20,16 @@ public class BorrowController {
     @Resource
     BorrowService borrowService;
 
+    //使用HystrixCommand指定备选方案
+    @HystrixCommand(fallbackMethod = "onError")
     @RequestMapping("/borrow/{uid}")
     UserBorrowDetail findUserBorrow(@PathVariable("uid") int uid){
         return borrowService.getUserBorrowDetailByUid(uid);
+    }
+
+    //备选方案，这里直接返回空列表
+    //注意参数和返回值要和上面一样
+    UserBorrowDetail onError(int uid){
+        return new UserBorrowDetail(null, Collections.emptyList());
     }
 }
--- borrow-service/src/main/java/com/test/service/client/UserClient.java
+++ borrow-service/src/main/java/com/test/service/client/UserClient.java
@@ -5,8 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//@FeignClient(value = "userservice"  , fallback = UserFallbackClient.class)
 @FeignClient("userservice")
 public interface UserClient {
     @RequestMapping("/user/{uid}")
-    public User findUserById(@PathVariable("uid") int uid);
+    User findUserById(@PathVariable("uid") int uid);
 }
